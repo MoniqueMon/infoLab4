78962f73bd91214f6ee848a2c1608aa347c72ae3
MoniqueMon <moncrieffemonique@gmail.com>
2016-10-14 23:47:40 -0500

    Add files via upload

--- maze.js
+++ maze.js
@@ -1,8 +1,9 @@
 
 window.onload = function(){
 	var playing = false;
+	
 	var boundaries = document.getElementsByClassName("boundary");
-	for(var a=0; a< boundaries.length - 1; a++){
+	for(var a=0; a < boundaries.length - 1; a++){
 		boundaries[a].onmouseover = myLose;
 		
 	}
@@ -11,12 +12,24 @@ window.onload = function(){
 	for(var a=0; a< boundaries.length-1; a++){
 		if(playing)
 		{
-			boundaries[i].setAttribute("class", "boundary youlose");
+			boundaries[a].setAttribute("class", "boundary youlose");
 		}
 	}
 	}
 		
-	
+	function compmaze(){
+		if(playing == true) {
+			var check = document.querySelectorAll(".youlose");
+			if(check.length > 0)
+			{
+				document.getElementById("status").textContent = "YOU LOSE";
+			}else{
+				document.getElementById("status").textContent = "YOU WIN";
+				
+			}
+			playing = false;
+		}
+	}
 	
 	
 	
