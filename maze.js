c4ced5b8407c1c54d36cce51ffbe0b4a50553b80
MoniqueMon <moncrieffemonique@gmail.com>
2016-10-14 22:06:52 -0500

    Add files via upload

--- maze.js
+++ maze.js
@@ -1,8 +1,5 @@
 window.onload = function() {
-    $("start").onclick = startClick;
-    $("end").onmouseover = overEnd;
-    var boundaries = $$("div#maze div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].onmouseover = overBoundary;
-    }
+    var boundaries = document.getElementsByClassName("boundary1");
+	for(var i=0; i< boundaries.length - 1; i++){
+		boundaries[i].onmouseover = youLose;
 }
