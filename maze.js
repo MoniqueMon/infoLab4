ee43db4d87cd582b22586a372d8f4dd16207f186
MoniqueMon <moncrieffemonique@gmail.com>
2016-10-14 21:27:29 -0500

    Add files via upload

--- maze.js
+++ maze.js
@@ -1,10 +1,8 @@
-window.onload = function ()
-{
+window.onload = function(){
 	$("boundary1").onmouseover = overBoundary;
 };
 
-function overBoundary ()
-{
+function overBoundary(){
 	//$("boundary1").style.backgroundColor = "#FF0000";
 	$("boundary1").addClassName("youlose");
 }
